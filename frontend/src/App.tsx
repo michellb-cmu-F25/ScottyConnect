151836898a7321bf8e04d8487f7e7fc4291b0847
JessieYu <jessie.yu3345@gmail.com>
2026-04-03 01:53:05 -0700

    feat: create frontend for main page and add placeholder for future routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
+import MainPage from './pages/MainPage'
+import PlaceholderPage from './pages/PlaceholderPage'
 
 function App() {
   return (
@@ -8,6 +10,12 @@ function App() {
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/mainpage" element={<MainPage />} />
+        <Route path="/networking" element={<PlaceholderPage title="Networking" />} />
+        <Route path="/feedback" element={<PlaceholderPage title="Feedback" />} />
+        <Route path="/publish-event" element={<PlaceholderPage title="Publish new event" />} />
+        <Route path="/attendance" element={<PlaceholderPage title="Attendance" />} />
+        <Route path="/my-events" element={<PlaceholderPage title="My events" />} />
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </BrowserRouter>
