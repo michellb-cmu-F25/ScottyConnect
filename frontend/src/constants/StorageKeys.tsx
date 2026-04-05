3007e92e69099c30607625934c9a07b4a6aba445
xshi3-cmu-F25 <xshi3@andrew.cmu.edu>
2026-04-05 15:39:03 -0700

    refactor: remove redundant file

--- frontend/src/constants/StorageKeys.tsx
+++
@@ -1,4 +0,0 @@
-// Storage keys for local storage
-export const PENDING_VERIFICATION_USER_KEY = 'scottyConnectPendingVerificationUser'
-export const TOKEN_KEY = 'scottyConnectToken'
-export const USER_KEY = 'scottyConnectUser'
