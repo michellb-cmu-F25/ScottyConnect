919ea5c0f9e62b721c2ec42736ab02a6a15bbdf3
xshi3-cmu-F25 <xshi3@andrew.cmu.edu>
2026-04-21 23:40:23 -0700

    feat: enforce andrew.cmu.edu account registration

--- backend/app/accounts/service.py
+++ backend/app/accounts/service.py
@@ -69,6 +69,10 @@ class AccountService(Service):
         if request.password != request.confirm_password:
             return RegisterResponse(message="Passwords do not match", user=None, code=400)
 
+        # Check if email ends in @andrew.cmu.edu
+        if not request.email.endswith("@andrew.cmu.edu"):
+            return RegisterResponse(message="Must be a CMU affiliated email (@andrew.cmu.edu)", user=None, code=400)
+
         # Create user
         user = User(
             username=request.username,
